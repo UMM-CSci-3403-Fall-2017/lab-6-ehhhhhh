ce8fcc286a07abd835f3bbd2962356bead827a9d
hamme503 <hamme503@morris.umn.edu>
2017-11-07 15:20:48 -0600

    add client and server files

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,31 @@
+import java.net.*;
+import java.io.*;
+
 package echoserver;
 
 public class EchoClient {
+	public static void main(String[] args) {
+		
+		hostname = args[0];
+
+		try {
+			Socket socket = new Socket("localhost", 1886);
+
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			socket.close();
+		} catch (IOException ioe) {
+			System.out.println("We caught an IOException");
+			System.err.println(ioe);
+		}
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,27 @@
+import java.net.*;
+import java.io.*;
+
 package echoserver;
 
 public class EchoServer {
+	public static void main(String[] args) {
+		try {
+			ServerSocket sock = new ServerSocket(1886);
+
+			while (true) {
+				System.out.println("Got a request!");
+				Socket client = sock.accept();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream));
+				PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					writer.println(line);
+				}
+			}
+		} catch (IOException ioe) {
+			System.err.println(ioe);
+		}
+	}
 }
